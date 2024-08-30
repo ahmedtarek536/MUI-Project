b025e273e0b7bf8641cb82af25e405993813c6fb
Ahmed Tarek <123866562+ahmedtarek536@users.noreply.github.com>
2024-08-30 12:53:18 +0300

    upload project

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import Navbar from "./components/Navbar";
+import Rightbar from "./components/Rightbar";
+import Sidebar from "./components/Sidebar";
+import Feed from "./components/Feed";
+import { Box, Stack } from "@mui/material";
+import Add from "./components/Add";
+
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import { useState } from "react";
+
+function App() {
+  const [mode, setMode] = useState("light");
+  const darkTheme = createTheme({
+    palette: {
+      mode: mode,
+    },
+  });
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <Box>
+        <Navbar />
+        <Stack direction="row" spacing={2} justifyContent="space-between">
+          <Sidebar mode={mode} setMode={setMode} />
+          <Feed />
+          <Rightbar />
+        </Stack>
+        <Add />
+      </Box>
+    </ThemeProvider>
+  );
+}
+
+export default App;
